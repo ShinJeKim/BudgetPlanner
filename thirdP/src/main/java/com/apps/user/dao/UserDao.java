60740e3513c740f64942131f873de50b6e2fa3cd
lchyeon90 <lchyeon90@gmail.com>
2017-09-22 12:42:49 +0900

    Create UserDao, UserDaoImpl

--- thirdP/src/main/java/com/apps/user/dao/UserDao.java
+++ thirdP/src/main/java/com/apps/user/dao/UserDao.java
@@ -1,5 +1,14 @@
 package com.apps.user.dao;
 
-public interface UserDao {
+import com.apps.common.WorkDiv;
+
+/**
+ * @since 2017-09-22
+ * @author 임채현
+ * @version 1.0
+ * UserDao extends WorkDiv
+ */
+
+public interface UserDao extends WorkDiv {
 
 }
--- thirdP/src/main/java/com/apps/user/dao/UserDaoImpl.java
+++ thirdP/src/main/java/com/apps/user/dao/UserDaoImpl.java
@@ -1,5 +1,52 @@
 package com.apps.user.dao;
 
-public class UserDaoImpl implements UserDao{
+import java.util.List;
+
+import com.apps.common.DTO;
+
+/**
+ * @since 2017-09-22
+ * @author 임채현
+ * @version 1.0
+ * UserDaoImpl implements UserDao
+ *
+ */
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public int do_save(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<?> do_search(DTO dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int do_delete(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int do_update(DTO dto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<?> do_excelDown() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int do_excelUp(List<?> list) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
